c3d942754761e9d002f65f19cf7df9b139aa7ddd
Jared <jareddj34@gmail.com>
2024-01-29 16:12:23 -0500

    added html head stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,17 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <Head>
+                <title>Jared de Monteiro</title>
+                <meta
+                    name="description"
+                    content="Jared de Monteiro's personal website"
+                />
+                <meta
+                    name="keywords"
+                    content="Jared, de Monteiro, computer science, game developer"
+                />
+            </Head>
             <body className="min-h-screen pt-12 bg-slate-50 antialiased">
                 <Navbar />
                 <div className="container max-w-7xl mx-auto h-full pb-4">
