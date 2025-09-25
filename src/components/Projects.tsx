b54fad9d392ace331150161c9141133cd1d8c906
Jared de Monteiro <jareddj34@gmail.com>
2025-09-24 22:54:08 -0400

    added airboat and temikki projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,13 +32,33 @@ const projects = [
         youtubelink: "",
     },
     {
-        name: "OriginPC Custom PC Builder Page",
+        name: "Origin PC Website",
         description:
-            "One of the web pages I made for OriginPC, showcasing our customizable aspects of our gaming PCs.",
+            "Updated and maintained the Origin PC website. Developed a new page showcasing the customizable aspects of the PCs.",
         image: "/images/OriginPC.png",
         github: "",
         link: "https://www.originpc.com/gaming/desktops/custom-pc-builder/",
         piclink: "https://www.originpc.com/gaming/desktops/custom-pc-builder/",
+        youtubelink: "",
+    },
+    {
+        name: "Swampocalypse",
+        description:
+            "A VR airboat driving experience through an apocalyptic Everglades utilizing a custom made haptic rig.",
+        image: "/images/swampocalypse.jpg",
+        github: "",
+        link: "https://xr.miami.edu/research/faculty-projects/swampocalypse/index.html",
+        piclink: "https://xr.miami.edu/research/faculty-projects/swampocalypse/index.html",
+        youtubelink: "",
+    },
+    {
+        name: "Temikki Garden",
+        description:
+            "A mobile AR application to promote the emotional well-being of children during long hospital stays. Children can build, modify, and play in a living virtual garden while reminded of their accomplishments.",
+        image: "/images/temikki.jpg",
+        github: "",
+        link: "https://xr.miami.edu/research/faculty-projects/temikki-garden/index.html",
+        piclink: "https://xr.miami.edu/research/faculty-projects/temikki-garden/index.html",
         youtubelink: "",
     },
     {
@@ -61,16 +81,16 @@ const projects = [
         piclink: "https://youtu.be/FMKl4py7z60",
         youtubelink: "https://youtu.be/FMKl4py7z60",
     },
-    // {
-    //     name: "JamZ",
-    //     description:
-    //         "A document simplifier, making it easy for people to read long, confusing documents.",
-    //     image: "/images/JamZ.png",
-    //     github: "https://github.com/jareddj34/JamZ",
-    //     link: "",
-    //     piclink: "https://github.com/jareddj34/JamZ",
-    //     youtubelink: "",
-    // },
+    {
+        name: "JamZ",
+        description:
+            "A document simplifier, making it easy for people to read long, confusing documents.",
+        image: "/images/JamZ.png",
+        github: "https://github.com/jareddj34/JamZ",
+        link: "",
+        piclink: "https://github.com/jareddj34/JamZ",
+        youtubelink: "",
+    },
 ];
 
 const figmas = [
@@ -187,36 +207,23 @@ const Projects = () => {
                                         {project.description}
                                     </p>
                                     <div className="flex flex-row space-x-4 ">
-                                        {project.name !=
-                                            "OriginPC Custom PC Builder Page" &&
-                                            project.name !=
-                                                "Flower Adventures" &&
-                                            project.name !=
-                                                "Gambling Animation" && (
-                                                <Link
-                                                    href={project.github}
-                                                    target="_blank"
+                                        {project.github && (
+                                            <Link href={project.github} target="_blank">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    x="0px"
+                                                    y="0px"
+                                                    width="50"
+                                                    height="50"
+                                                    viewBox="0 0 24 24"
+                                                    className="hover-float"
                                                 >
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        x="0px"
-                                                        y="0px"
-                                                        width="50"
-                                                        height="50"
-                                                        viewBox="0 0 24 24"
-                                                        className="hover-float"
-                                                    >
-                                                        <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
-                                                    </svg>
-                                                </Link>
-                                            )}
+                                                    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+                                                </svg>
+                                            </Link>
+                                        )}
 
-                                        {(project.name ===
-                                            "OriginPC Custom PC Builder Page" ||
-                                            project.name ===
-                                                "Kappa Theta Pi Website" ||
-                                            project.name ==
-                                                "Flower Adventures") && (
+                                        {project.link && (
                                             <Link
                                                 href={project.link}
                                                 target="_blank"
